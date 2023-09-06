2722d2cc5b6c45c0e9fd27a8fe53bca7daa925c0
Holger Castillo <hcastillo9498@gmail.com>
2023-09-06 21:28:21 +0200

    create toolbar and init base components

--- Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/app-routing.module.ts
+++ Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'crud', component: CrudComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'Profile', component: ProfileComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/app.module.ts
+++ Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/app.module.ts
@@ -4,10 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { AboutComponent } from './components/about/about.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        CrudComponent,
+        DashboardComponent,
+        GalleryComponent,
+        HomeComponent,
+        LoginComponent,
+        ProfileComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatDividerModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
