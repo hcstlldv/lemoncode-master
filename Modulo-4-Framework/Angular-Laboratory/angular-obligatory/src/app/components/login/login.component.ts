1d4009f02e9351959ba67895deb130db5f4d3ba9
Holger Castillo <hcastillo9498@gmail.com>
2023-09-13 21:57:27 +0200

    create login form

--- Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/components/login/login.component.ts
+++ Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
     templateUrl: 'login.component.html',
 })
 export class LoginComponent implements OnInit {
-    constructor() {}
+    loginForm: FormGroup | undefined;
 
-    ngOnInit() {}
+    constructor(private readonly _formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.loginForm = this._formBuilder.group({
+            name: this._formBuilder.control('', {
+                validators: [Validators.required],
+            }),
+            password: this._formBuilder.control('', {
+                validators: [Validators.required],
+            }),
+        });
+    }
 }
