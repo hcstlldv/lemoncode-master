3549e3304abbc7ba76e0c0a444fa0c391860d18b
Holger Castillo <hcastillo9498@gmail.com>
2023-09-13 22:24:30 +0200

    add isLogged global state and hide private section of main nav when user is not logged

--- Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/components/login/auth.service.ts
+++ Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/components/login/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 type User = {
     name: string;
@@ -7,18 +8,25 @@ type User = {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    private readonly isLogged$ = new BehaviorSubject<boolean>(false);
+
     constructor() {}
 
     login(user: User): boolean {
         const { password, name } = user;
         if (password !== '12345678') return false;
         if (name !== 'master@lemoncode.net') return false;
+        this.isLogged$.next(true);
         return true;
     }
 
-    logout() {}
+    logout() {
+        this.isLogged$.next(false);
+    }
 
-    isLogged() {}
+    isLogged(): Observable<boolean> {
+        return this.isLogged$.asObservable();
+    }
 
     getUsername() {}
 }
