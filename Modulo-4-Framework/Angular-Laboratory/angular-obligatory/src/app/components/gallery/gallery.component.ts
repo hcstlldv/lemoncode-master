5d2f8e064c796856a1a9ff171f14c3007995168a
Holger Castillo <hcastillo9498@gmail.com>
2023-09-13 23:32:08 +0200

    add main photo and gallery of photos list

--- Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/components/gallery/gallery.component.ts
+++ Modulo-4-Framework/Angular-Laboratory/angular-obligatory/src/app/components/gallery/gallery.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+type Image = {
+    id: number;
+    src: string;
+    title: string;
+};
+
 @Component({
     selector: 'app-gallery',
     templateUrl: 'gallery.component.html',
+    styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+    currentImageId: number = 1;
+
+    images: Image[] = [
+        { id: 1, src: 'img1', title: 'image 1' },
+        { id: 2, src: 'img2', title: 'image 2' },
+        { id: 3, src: 'img3', title: 'image 3' },
+        { id: 4, src: 'img4', title: 'image 4' },
+        { id: 5, src: 'img5', title: 'image 5' },
+        { id: 6, src: 'img6', title: 'image 6' },
+        { id: 7, src: 'img7', title: 'image 7' },
+        { id: 8, src: 'img8', title: 'image 8' },
+        { id: 9, src: 'img9', title: 'image 9' },
+    ];
     constructor() {}
 
     ngOnInit() {}
+
+    trackByFn(index: number, item: Image) {
+        return item.id;
+    }
 }
