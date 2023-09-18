a8c00ca355c70641e9acb3cddf789e4e8c07e4a4
Holger Castillo <hcastillo9498@gmail.com>
2023-09-18 16:22:59 +0200

    refactor styles to add key to each item of the list

--- Modulo-4-Framework/React-Laboratory/src/list.tsx
+++ Modulo-4-Framework/React-Laboratory/src/list.tsx
@@ -18,19 +18,24 @@ export const ListPage: React.FC = () => {
 
     return (
         <>
-            <h2>Hello from List page</h2>+{' '}
+            <h2>Hello from List page</h2>{' '}
             <div className="list-user-list-container">
-                <span className="list-header">Avatar</span>
-                <span className="list-header">Id</span>
-                <span className="list-header">Name</span>
+                <div className="list-header">
+                    <span className="avatar-header">Avatar</span>
+                    <span>Id</span>
+                    <span className="name-header">Name</span>
+                </div>
                 {members.map((member) => (
-                    <>
-                        <img src={member.avatar_url} />
+                    <div className="list-item" key={member.id}>
+                        <img className="avatar-item" src={member.avatar_url} />
                         <span>{member.id}</span>
-                        <Link to={`/detail/${member.login}`}>
+                        <Link
+                            className="name-item"
+                            to={`/detail/${member.login}`}
+                        >
                             {member.login}
                         </Link>
-                    </>
+                    </div>
                 ))}
             </div>
             <Link to="/detail">Navigate to detail page</Link>
